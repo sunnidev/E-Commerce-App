05a704f261ad6c856a713f1823a7657975fcf27f
Sunni Dev <officialsunnidev@gmail.com>
2026-06-20 15:23:13 +0500

    feat: add splash screen assets and configuration for dark mode support
    - Added splash screen images for various resolutions and modes (light/dark).
    - Implemented launch background XML files for Android with layered backgrounds.
    - Updated iOS launch images and storyboard to support dark mode.
    - Configured Flutter Native Splash for a seamless splash experience.
    - Modified pubspec.yaml to include flutter_native_splash dependency.
    - Enhanced web splash screen with responsive images for light and dark themes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_app/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding:widgetsBinding );
   runApp(const MyApp());
 }
